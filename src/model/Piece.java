1c202686a239c6a9ab91fb1135e646dfd085246a
H4CK3R5 <75364799+H4CK3R5-FS@users.noreply.github.com>
2021-11-18 16:00:45 +0100

    Update Piece.java

--- src/model/Piece.java
+++ src/model/Piece.java
@@ -1,46 +1,46 @@
 package model;
 
 public class Piece {
-String name,colour;
-int id,y,x;
-public String getName() {
-	return name;
-}
-public Piece(String name, String colour, int id, int y, int x) {
-	super();
-	this.name = name;
-	this.colour = colour;
-	this.id = id;
-	this.y = y;
-	this.x = x;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getColour() {
-	return colour;
-}
-public void setColour(String colour) {
-	this.colour = colour;
-}
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public int getY() {
-	return y;
-}
-public void setY(int y) {
-	this.y = y;
-}
-public int getX() {
-	return x;
-}
-public void setX(int x) {
-	this.x = x;
-}
+	String name,colour;
+	int id,y,x;
+	public String getName() {
+		return name;
+	}
+	public Piece(String name, String colour, int id, int y, int x) {
+		super();
+		this.name = name;
+		this.colour = colour;
+		this.id = id;
+		this.y = y;
+		this.x = x;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
 
 
 
